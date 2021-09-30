ae6b485f90e56ea0041556cb132a38c252ac85f9
Toby Ho <airportyh@gmail.com>
2021-09-30 09:59:05 -0400

    plot points

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,5 +1,26 @@
 const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 const width = 500;
 const height = 500;
+
+plotPoint(250, 250);
+
+canvas.addEventListener("click", (event) => {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log("x", x, "y", y);
+    plotPoint(x, y);
+});
+
+function plotPoint(x, y, radius=5) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+
+
+
+
 
